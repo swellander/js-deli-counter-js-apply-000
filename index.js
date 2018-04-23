f4fc3d83419c07a775e508dc05687b43908fb63f
Sam Wellander <samwellander@gmail.com>
2018-04-23 20:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+var katzDeli = [];
+
 function takeANumber(line, name) {
   line.push(name);
   return 'Welcome, ' + name + '. You are number ' + line.length + ' in line.'
+}
+
+function nowServing() {
+  if (katzDeli.length < 1) {
+    return 'There is nobody waiting to be served!';
+  }
 }
