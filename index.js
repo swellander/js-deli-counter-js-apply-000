5fc774d67898935219787a37edff4f65e4d088d8
Sam Wellander <samwellander@gmail.com>
2018-04-23 20:15:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing() {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
   } else {
-    return katzDeliLine.shift();
+    return 'Currently serving ' + katzDeliLine[0] + '.';
   }
 }
 
